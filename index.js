cf645b941efa6dadf4863f154ddf9166b7c795df
Greg Damico <greg.damico@flatironschool.com>
2022-06-07 13:46:24 -0700

    commit tree c4045644481cbc7c30ddcf6ff3440b9daa1972f9

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let multiply;
+const num1 = 2, num2 = 31;
+multiply = num1 * num2;
 
-// Write your code here
+let random;
+random = Math.floor(Math.random() + 1);
+
+let mod;
+const num3 = 19, num4 = 5;
+mod = num3 % num4;
+
+let max;
+const set = (1, 20);
+max = Math.max(set);
